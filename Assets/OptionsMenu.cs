a8747bed70f0e51fb87b3f24068282b35b37fad0
Haydrill <saldua.h@gmail.com>
2021-05-09 14:15:15 -0700

    Fixed volume and sensitivity sliders
    Volume and Sensitivity sliders now save settings across scenes

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -1,13 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public Look player;
+    /*public Look player;
 
     public void SetSensitivity(float sens)
     {
         player.mouseSensitivity = sens;
+    }
+    */
+    public void SaveVolume(float value)
+    {
+        PlayerPrefs.SetFloat("volume", value);
+    }
+
+
+    public Slider volume;
+    public Slider sens;
+    public void SetSliders()
+    {
+        volume.value = PlayerPrefs.GetFloat("volume", .3f);
+        sens.value = PlayerPrefs.GetFloat("sens", .21f);
+    }
+
+    //public TextMeshProUGUI volumeText;
+    public TextMeshProUGUI sensText;
+
+    public void SetText()
+    {
+        //volumeText.text = PlayerPrefs.GetFloat("volume", .3f).ToString();
+        sensText.text = PlayerPrefs.GetFloat("sens", 21f).ToString();
+    }
+
+    public void Awake()
+    {
+        SetSliders();
+        SetText();
+        Debug.Log("Awaken");
     }
 }
---
+++ Assets/SettingMgr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingMgr : MonoBehaviour
+{
+    public void Start()
+    {
+        // Default values for the preferences
+        //PlayerPrefs.SetFloat("volume", 30f);
+        //PlayerPrefs.SetFloat("sens", 21f);
+        //PlayerPrefs.Save();
+    }
+
+    public void SaveVolume(Slider slider)
+    {
+        Debug.Log(PlayerPrefs.GetFloat("volume", 6969f));
+        PlayerPrefs.SetFloat("volume", slider.value);
+        PlayerPrefs.Save();
+        Debug.Log("Saved");
+    }
+
+    public void SaveSensitivity(Slider slider)
+    {
+        PlayerPrefs.SetFloat("sens", slider.value);
+        PlayerPrefs.Save();
+    }
+
+    float GetVolume()
+    {
+        return PlayerPrefs.GetFloat("volume", 30f);
+    }
+
+    float GetSensitivity()
+    {
+        return PlayerPrefs.GetFloat("sens", 21f);
+    }
+}
